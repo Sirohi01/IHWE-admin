9ae18750de1a6ca1f2557c62d2392b1611b162c8
Vansh Chaudhary <vansh.2002vc@gmail.com>
2026-07-10 13:45:27 +0530

    admin optimise

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression';
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    viteCompression({ algorithm: 'gzip', ext: '.gz' }),
+    viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
+    visualizer({ open: false, filename: 'stats.html', gzipSize: true, brotliSize: true })
+  ],
   server: {
     proxy: {
       '^/.*\\.(xml|txt|html)$': {
@@ -12,5 +20,8 @@ export default defineConfig({
         changeOrigin: true,
       },
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
   }
 })
